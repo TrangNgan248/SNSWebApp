af31be03ff1e06224dbb481254c674fd98f09f27
Tuan Anh <tuananhchums23@gmail.com>
2022-06-19 10:18:42 +0700

    Chi tiet Post

--- sns/src/components/feed/feed.js
+++ sns/src/components/feed/feed.js
@@ -1,6 +1,11 @@
+import Post from "../post/Post"
 import "./feed.css"
 export default function Feed(){
     return(
-        <div className="feed"> Feed </div>
+        <div className="feed"> 
+            <div className="feedWrapper">
+                <Post/>
+            </div>
+        </div>
     )
 }
