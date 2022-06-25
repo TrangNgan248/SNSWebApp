30b315f3f3ffc4c249f1cb554de04102d7ccf9c4
Tran Minh Quan <tranminhquan366@gmail.com>
2022-06-25 17:53:04 +0700

    add share button, picture,title

--- sns/src/Pages/Home/Home.js
+++ sns/src/Pages/Home/Home.js
@@ -4,7 +4,6 @@ import Feed from "../../components/feed/feed"
 import Rightbar from "../../components/rightbar/rightbar"
 import { useNavigate } from "react-router-dom"
 import "./Home.css"
-import { Link } from "react-router-dom"
 
 export default function Home() {
     const navigate = useNavigate()
