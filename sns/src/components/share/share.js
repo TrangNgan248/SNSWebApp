0aee2b73f767443037aed1a583484fd3af884d64
Your Name <you@example.com>
2022-06-26 07:56:45 +0700

    bỏ nhập user_id và author_id

--- sns/src/components/share/share.js
+++ sns/src/components/share/share.js
@@ -7,20 +7,16 @@ function AddPost(){
   const [title, setTitle]= useState("");
   const [display, setDisplay] = useState("");
   const [content, setContent] = useState("");
-  const [author_id, setAuthor_id] = useState("");
-  const [channel_id, setChannel_id] = useState("");
-  const [num_view, setNum_view] = useState("");
+
     async function addPost(){ 
 
-    let item = {title,content,display,author_id,channel_id,num_view}
+    let item = {title,content,display}
     console.warn(item)
     const formData = new FormData();
     formData.append('display', display);
     formData.append('content', content);
     formData.append('title', title);
-    formData.append('author_id', author_id);
-    formData.append('channel_id', channel_id);
-    formData.append('num_view', num_view);
+
     await fetch("http://localhost:8000/api/post/create" , {
         method: 'POST',
         body: formData
@@ -37,9 +33,7 @@ function AddPost(){
             <input type="text" placeholder="Title" className="shareInput" onChange={(e) =>setTitle(e.target.value)}  />
             <input type="text" placeholder="What's in your mind ?" className="shareInput1" onChange={(e) =>setContent(e.target.value)} />
             <input type="file" placeholder="Picture" className="shareInput2" onChange={(e) =>setDisplay(e.target.files[0])}/>
-            <input type="text" placeholder="author_id" className="shareInput" onChange={(e) =>setAuthor_id(e.target.value)}  />
-            <input type="text" placeholder="setChannel_id" className="shareInput" onChange={(e) =>setChannel_id(e.target.value)}  />
-            <input type="text" placeholder="setNum_view" className="shareInput" onChange={(e) =>setNum_view(e.target.value)}  />
+  
             <button onClick={addPost} className="btn btn-primary">post </button>
           </div>
 
