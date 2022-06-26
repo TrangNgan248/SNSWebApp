a7415410b327dc3a49489a1d2e898b8998ce8369
hao <nguyenvanhao12902@gmail.com>
2022-06-27 06:34:32 +0700

    update xem bai viet sua edit

--- sns-backend/app/Http/Controllers/ChannelController.php
+++ sns-backend/app/Http/Controllers/ChannelController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Channel;
 
 class ChannelController extends Controller
 {
-    //
+    public function index(){
+        $channels = Channel::all();
+        return $channels;
+    }
 }
