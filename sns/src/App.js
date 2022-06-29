a5e758c28862b0629096abc701a68f058f52d524
Tuan Anh <tuananhchums23@gmail.com>
2022-06-29 08:07:12 +0700

    commit de pull lai

--- sns/src/App.js
+++ sns/src/App.js
@@ -12,7 +12,6 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
-          <Route path="/detail" element={<Detail/>}/>
           <Route path="/edit" element={<Edit/>}/>
         </Routes> 
       </Router>
---
+++ sns/src/Pages/Seek/seek.js
@@ -0,0 +1,16 @@
+// import Topbar from "../../components/topbar/Topbar"
+// import Sidebar from "../../components/sidebar/sidebar"
+// import Feed from "../../components/feed/feed"
+// import Rightbar from "../../components/rightbar/rightbar"
+// import { useNavigate } from "react-router-dom"
+ import "./seek.css"
+//<seekTopbar />
+export default function Seek() {
+    return (
+        <>
+            
+            <div className="homeContainer">
+            </div>
+        </>
+    )
+};
